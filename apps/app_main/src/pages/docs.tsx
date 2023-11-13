0424e6087e60a48a953d69ea3b9cbf23048946f4
stepover10 <stepover10@gmail.com>
2023-11-13 16:12:47 +0900

    fix(main): MF 기본 세팅

--- apps/app_main/src/pages/docs.tsx
+++ apps/app_main/src/pages/docs.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import RemoteNextPage from "app_docs/index";
-
 const Page_Docs: any = () => {
   return (
     <div>
@@ -9,15 +8,14 @@ const Page_Docs: any = () => {
     </div>
   )
 }
-
 export default Page_Docs;
 
 Page_Docs.getInitialProps = RemoteNextPage.getInitialProps;
 
 /*
-import RemoteDocsIndex from 'app_docs/index';
-const Page_Docs = RemoteDocsIndex;
-Page_Docs.getInitialProps = RemoteDocsIndex.getInitialProps;
-export default Page_Docs;
+  import RemoteDocsIndex from 'app_docs/index';
+  const Page_Docs = RemoteDocsIndex;
+  Page_Docs.getInitialProps = RemoteDocsIndex.getInitialProps;
+  export default Page_Docs;
 */
 
